db6a86e3f065a3a7c7c1e91b4a7634090091053b
nata-goa/vivi <bajelidzenata0@gmail.com>
2024-12-14 01:06:35 +0400

    second hw

---
+++ level 01/homeworkk -/hw1.py
@@ -0,0 +1,35 @@
+name = "natali"
+#name არის ცვლადი
+# = არის ცვლადისთვის მიმნიჭებელი სიმბოლო
+# "natali" არის ცვლადისტვის მნისვნელობა
+
+surname ="bajelidze"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "natali" #ეს არის str (string) ტიპის ცვლადი
+age = 15 # ეს არის int (integar) მთელი რიცხვი
+height = 168 #ეს არის float ტიპის ცვლადი ( )
+#boolean (booL) ტიპის ცვლადი
+
+knows_programming = True  #True ან False
+is_ugly = False  #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული comeLcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(heighte))
+# print(type(knows_programming))
+
+print(name + "" + str(age))
+
+#mevarnatalichemigvariabajelidzevar15wlis
